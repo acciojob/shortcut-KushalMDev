b9b46c7b7910a3fcec04f87d5f070c39e4c912fd
KushalMDev <105456987+KushalMDev@users.noreply.github.com>
2024-03-25 20:56:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function shortcut(s1, s2) {
   // your code here
+	s1=s1.trim()
+    s2=s2.trim()
+    if(s1.length==0 || s2.length==0)return ''
+    return s1[0]+s2[0]
 }
 
 // Do not change the code below.
